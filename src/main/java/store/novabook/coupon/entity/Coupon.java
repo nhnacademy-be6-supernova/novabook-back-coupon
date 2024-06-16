b8a9ffb7b262c352ccab5ac66373c1cdd0c8fa14
zaysverse <zeze@jbnu.ac.kr>
2024-06-16 19:51:33 +0900

    style: 정렬

--- src/main/java/store/novabook/coupon/entity/Coupon.java
+++ src/main/java/store/novabook/coupon/entity/Coupon.java
@@ -14,31 +14,41 @@ import jakarta.persistence.Id;
 @Entity
 public class Coupon {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@Column(nullable = false, length = 255)
-	private String name;
-	@Column(nullable = false)
-	private BigDecimal discountAmount;
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false, length = 50)
-	private DiscountType discountType;
-	@Column(nullable = false)
-	private BigDecimal maxDiscountAmount;
-	@Column(nullable = false)
-	private BigDecimal minPurchaseAmount;
-	@Column(nullable = false)
-	private ZonedDateTime startedAt;
-	@Column(nullable = false)
-	private ZonedDateTime expirationAt;
-	@Column(nullable = false)
-	private ZonedDateTime createdAt = ZonedDateTime.now();
-	@Column
-	private ZonedDateTime updatedAt;
-
 	public enum DiscountType {
 		AMOUNT,
 		PERCENT
 	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 255)
+	private String name;
+
+	@Column(nullable = false)
+	private BigDecimal discountAmount;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 50)
+	private DiscountType discountType;
+
+	@Column(nullable = false)
+	private BigDecimal maxDiscountAmount;
+
+	@Column(nullable = false)
+	private BigDecimal minPurchaseAmount;
+
+	@Column(nullable = false)
+	private ZonedDateTime startedAt;
+
+	@Column(nullable = false)
+	private ZonedDateTime expirationAt;
+
+	@Column(nullable = false)
+	private ZonedDateTime createdAt = ZonedDateTime.now();
+
+	@Column
+	private ZonedDateTime updatedAt;
+
 }
