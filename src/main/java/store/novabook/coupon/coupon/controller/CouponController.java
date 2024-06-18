adb8b9a3a2dafbf64814581a1fca1f59467be2f5
zaysverse <zeze@jbnu.ac.kr>
2024-06-18 17:18:36 +0900

    #4 add: read 컨트롤러 추가

--- src/main/java/store/novabook/coupon/coupon/controller/CouponController.java
+++ src/main/java/store/novabook/coupon/coupon/controller/CouponController.java
@@ -2,7 +2,8 @@ package store.novabook.coupon.coupon.controller;
 
 import java.awt.print.Pageable;
 
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
@@ -20,6 +20,9 @@ import store.novabook.coupon.coupon.dto.request.CreateCouponCategoryRequest;
 import store.novabook.coupon.coupon.dto.request.CreateCouponRequest;
 import store.novabook.coupon.coupon.dto.request.UpdateCouponExpirationRequest;
 import store.novabook.coupon.coupon.dto.response.CreateCouponResponse;
+import store.novabook.coupon.coupon.dto.response.GetCouponBookResponse;
+import store.novabook.coupon.coupon.dto.response.GetCouponCategoryResponse;
+import store.novabook.coupon.coupon.dto.response.GetCouponResponse;
 import store.novabook.coupon.coupon.service.CouponService;
 
 @RestController
@@ -55,11 +58,21 @@ public class CouponController {
 	}
 
 	@GetMapping
-	public ResponseEntity<Page<ReadCouponResponse>> getCouponAll(
-		@RequestParam(defaultValue = "0") int page,
-		@RequestParam(defaultValue = "10") int size) {
-		Pageable pageable = PageRequest.of(page, size);
-		Page<ReadCouponResponse> coupons = couponService.getAllCoupons(pageable);
+	public ResponseEntity<Page<GetCouponResponse>> getCouponGeneralAll(@PageableDefault(size = 10) Pageable pageable) {
+		Page<GetCouponResponse> coupons = couponService.getCouponGeneralAll(pageable);
+		return ResponseEntity.ok(coupons);
+	}
+
+	@GetMapping
+	public ResponseEntity<Page<GetCouponBookResponse>> getCouponBookAll(@PageableDefault(size = 10) Pageable pageable) {
+		Page<GetCouponBookResponse> coupons = couponService.getCouponBookAll(pageable);
+		return ResponseEntity.ok(coupons);
+	}
+
+	@GetMapping
+	public ResponseEntity<Page<GetCouponCategoryResponse>> getCouponCategoryAll(
+		@PageableDefault(size = 10) Pageable pageable) {
+		Page<GetCouponCategoryResponse> coupons = couponService.getCouponCategryAll(pageable);
 		return ResponseEntity.ok(coupons);
 	}
 
--- src/main/java/store/novabook/coupon/coupon/service/CouponService.java
+++ src/main/java/store/novabook/coupon/coupon/service/CouponService.java
@@ -1,10 +1,17 @@
 package store.novabook.coupon.coupon.service;
 
+import java.awt.print.Pageable;
+
+import org.springframework.data.domain.Page;
+
 import store.novabook.coupon.coupon.dto.request.CreateCouponBookRequest;
 import store.novabook.coupon.coupon.dto.request.CreateCouponCategoryRequest;
 import store.novabook.coupon.coupon.dto.request.CreateCouponRequest;
 import store.novabook.coupon.coupon.dto.request.UpdateCouponExpirationRequest;
 import store.novabook.coupon.coupon.dto.response.CreateCouponResponse;
+import store.novabook.coupon.coupon.dto.response.GetCouponBookResponse;
+import store.novabook.coupon.coupon.dto.response.GetCouponCategoryResponse;
+import store.novabook.coupon.coupon.dto.response.GetCouponResponse;
 
 public interface CouponService {
 
@@ -15,4 +22,10 @@ public interface CouponService {
 	CreateCouponResponse saveCategoryCoupon(CreateCouponCategoryRequest createCouponCategoryRequest);
 
 	void updateCouponExpiration(UpdateCouponExpirationRequest request);
+
+	Page<GetCouponResponse> getCouponGeneralAll(Pageable pageable);
+
+	Page<GetCouponBookResponse> getCouponBookAll(Pageable pageable);
+
+	Page<GetCouponCategoryResponse> getCouponCategryAll(Pageable pageable);
 }
