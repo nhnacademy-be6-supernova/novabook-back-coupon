f4c16855960acc586c4bb38d522871a76ca6ea44
zaysverse <zeze@jbnu.ac.kr>
2024-06-21 11:27:13 +0900

    refactor: "B","C"로 판별하는 두번조회하는 문 삭제

--- src/main/java/store/novabook/coupon/coupon/repository/querydsl/impl/CustomizedMemberCouponRepositoryImpl.java
+++ src/main/java/store/novabook/coupon/coupon/repository/querydsl/impl/CustomizedMemberCouponRepositoryImpl.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Projections;
@@ -21,6 +22,7 @@ import store.novabook.coupon.coupon.dto.MemberCategoryCouponDto;
 import store.novabook.coupon.coupon.dto.response.GetMemberCouponResponse;
 import store.novabook.coupon.coupon.repository.querydsl.CustomizedMemberCouponRepository;
 
+@Transactional(readOnly = true)
 public class CustomizedMemberCouponRepositoryImpl extends QuerydslRepositorySupport
 	implements CustomizedMemberCouponRepository {
 
@@ -35,8 +37,7 @@ public class CustomizedMemberCouponRepositoryImpl extends QuerydslRepositorySupp
 		QCategoryCoupon categoryCoupon = QCategoryCoupon.categoryCoupon;
 
 		BooleanBuilder builder = new BooleanBuilder();
-		builder.and(memberCoupon.memberId.eq(memberId))
-			.and(coupon.code.startsWith("C"));
+		builder.and(memberCoupon.memberId.eq(memberId));
 
 		if (validOnly) {
 			builder.and(coupon.startedAt.before(LocalDateTime.now()))
@@ -44,10 +45,12 @@ public class CustomizedMemberCouponRepositoryImpl extends QuerydslRepositorySupp
 				.and(memberCoupon.status.eq(MemberCouponStatus.UNUSED));
 		}
 
-		return from(categoryCoupon)
-			.select(Projections.constructor(MemberCategoryCouponDto.class, memberCoupon.id, categoryCoupon))
-			.innerJoin(coupon).on(categoryCoupon.coupon.code.eq(coupon.code))
-			.join(memberCoupon).on(memberCoupon.coupon.code.eq(coupon.code))
+		return from(categoryCoupon).select(
+				Projections.constructor(MemberCategoryCouponDto.class, memberCoupon.id, categoryCoupon))
+			.innerJoin(coupon)
+			.on(categoryCoupon.coupon.code.eq(coupon.code))
+			.join(memberCoupon)
+			.on(memberCoupon.coupon.code.eq(coupon.code))
 			.where(builder)
 			.fetch();
 	}
@@ -59,8 +62,7 @@ public class CustomizedMemberCouponRepositoryImpl extends QuerydslRepositorySupp
 		QBookCoupon bookCoupon = QBookCoupon.bookCoupon;
 
 		BooleanBuilder builder = new BooleanBuilder();
-		builder.and(memberCoupon.memberId.eq(memberId))
-			.and(coupon.code.startsWith("B"));
+		builder.and(memberCoupon.memberId.eq(memberId));
 
 		if (validOnly) {
 			builder.and(coupon.startedAt.before(LocalDateTime.now()))
@@ -68,10 +70,11 @@ public class CustomizedMemberCouponRepositoryImpl extends QuerydslRepositorySupp
 				.and(memberCoupon.status.eq(MemberCouponStatus.UNUSED));
 		}
 
-		return from(bookCoupon)
-			.select(Projections.constructor(MemberBookCouponDto.class, memberCoupon.id, bookCoupon))
-			.innerJoin(coupon).on(bookCoupon.coupon.code.eq(coupon.code))
-			.innerJoin(memberCoupon).on(bookCoupon.coupon.code.eq(memberCoupon.coupon.code))
+		return from(bookCoupon).select(Projections.constructor(MemberBookCouponDto.class, memberCoupon.id, bookCoupon))
+			.innerJoin(coupon)
+			.on(bookCoupon.coupon.code.eq(coupon.code))
+			.innerJoin(memberCoupon)
+			.on(bookCoupon.coupon.code.eq(memberCoupon.coupon.code))
 			.where(builder)
 			.fetch();
 	}
@@ -82,8 +85,7 @@ public class CustomizedMemberCouponRepositoryImpl extends QuerydslRepositorySupp
 		QCoupon coupon = QCoupon.coupon;
 
 		BooleanBuilder builder = new BooleanBuilder();
-		builder.and(memberCoupon.memberId.eq(memberId))
-			.and(coupon.code.startsWith("G"));
+		builder.and(memberCoupon.memberId.eq(memberId)).and(coupon.code.startsWith("G"));
 
 		if (validOnly) {
 			builder.and(coupon.startedAt.before(LocalDateTime.now()))
@@ -91,18 +93,12 @@ public class CustomizedMemberCouponRepositoryImpl extends QuerydslRepositorySupp
 				.and(memberCoupon.status.eq(MemberCouponStatus.UNUSED));
 		}
 
-		return from(memberCoupon)
-			.select(Projections.constructor(GetMemberCouponResponse.class,
-				memberCoupon.id,
-				coupon.code,
-				coupon.name,
-				coupon.discountAmount,
-				coupon.discountType,
-				coupon.maxDiscountAmount,
-				coupon.minPurchaseAmount,
-				coupon.startedAt,
-				coupon.expirationAt))
-			.innerJoin(coupon).on(memberCoupon.coupon.code.eq(coupon.code))
+		return from(memberCoupon).select(
+				Projections.constructor(GetMemberCouponResponse.class, memberCoupon.id, coupon.code, coupon.name,
+					coupon.discountAmount, coupon.discountType, coupon.maxDiscountAmount, coupon.minPurchaseAmount,
+					coupon.startedAt, coupon.expirationAt))
+			.innerJoin(coupon)
+			.on(memberCoupon.coupon.code.eq(coupon.code))
 			.where(builder)
 			.fetch();
 	}
@@ -113,25 +109,13 @@ public class CustomizedMemberCouponRepositoryImpl extends QuerydslRepositorySupp
 		QMemberCoupon memberCoupon = QMemberCoupon.memberCoupon;
 		QCoupon coupon = QCoupon.coupon;
 
-		return select(Projections.constructor(GetMemberCouponResponse.class,
-			memberCoupon.id.as("memberCouponId"),
-			coupon.code,
-			coupon.name,
-			coupon.discountAmount,
-			coupon.discountType,
-			coupon.maxDiscountAmount,
-			coupon.minPurchaseAmount,
-			coupon.startedAt,
-			coupon.expirationAt))
-			.from(memberCoupon)
+		return select(
+			Projections.constructor(GetMemberCouponResponse.class, memberCoupon.id.as("memberCouponId"), coupon.code,
+				coupon.name, coupon.discountAmount, coupon.discountType, coupon.maxDiscountAmount,
+				coupon.minPurchaseAmount, coupon.startedAt, coupon.expirationAt)).from(memberCoupon)
 			.join(memberCoupon.coupon, coupon)
-			.where(
-				memberCoupon.memberId.eq(memberId),
-				coupon.code.startsWith(prefix),
-				memberCoupon.status.eq(status),
-				coupon.expirationAt.lt(expirationAt),
-				coupon.startedAt.gt(startedAt)
-			)
+			.where(memberCoupon.memberId.eq(memberId), coupon.code.startsWith(prefix), memberCoupon.status.eq(status),
+				coupon.expirationAt.lt(expirationAt), coupon.startedAt.gt(startedAt))
 			.fetch();
 	}
 
