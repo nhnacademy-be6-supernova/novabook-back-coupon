0592d4daa6207de0ccaa2244c266f74f9b4f6e68
zaysverse <zeze@jbnu.ac.kr>
2024-06-21 17:14:24 +0900

    fix: valid를 Object로 바꿈
    - bean validation 작동안함

--- src/main/java/store/novabook/coupon/coupon/dto/request/CreateCouponBookRequest.java
+++ src/main/java/store/novabook/coupon/coupon/dto/request/CreateCouponBookRequest.java
@@ -12,13 +12,13 @@ import store.novabook.coupon.coupon.domain.Discountable;
 @ValidDiscount
 public record CreateCouponBookRequest(@NotNull(message = "책 ID는 필수 입력 항목입니다.") Long bookId,
 									  @NotNull(message = "이름은 필수 입력 항목입니다.") @Size(max = 255, message = "이름은 255자 이하로 입력해야 합니다.") String name,
-									  @NotNull(message = "할인 금액은 필수 입력 항목입니다.") long discountAmount,
+									  @NotNull(message = "할인 금액은 필수 입력 항목입니다.") Long discountAmount,
 									  @NotNull(message = "할인 유형은 필수 입력 항목입니다.") DiscountType discountType,
-									  @NotNull(message = "최대 할인 금액은 필수 입력 항목입니다.") long maxDiscountAmount,
-									  @NotNull(message = "최소 구매 금액은 필수 입력 항목입니다.") long minPurchaseAmount,
+									  @NotNull(message = "최대 할인 금액은 필수 입력 항목입니다.") Long maxDiscountAmount,
+									  @NotNull(message = "최소 구매 금액은 필수 입력 항목입니다.") Long minPurchaseAmount,
 									  @NotNull(message = "시작 날짜는 필수 입력 항목입니다.") LocalDateTime startedAt,
 									  @NotNull(message = "만료 날짜는 필수 입력 항목입니다.") LocalDateTime expirationAt,
-									  @NotNull(message = "사용 가능일은 필수 입력 항목입니다.") int usePeriod)
+									  @NotNull(message = "사용 가능일은 필수 입력 항목입니다.") Integer usePeriod)
 	implements Discountable {
 	@AssertTrue(message = "시작 날짜는 만료 날짜보다 이전이어야 합니다.")
 	public boolean isValidDates() {
--- src/main/java/store/novabook/coupon/coupon/dto/request/CreateCouponCategoryRequest.java
+++ src/main/java/store/novabook/coupon/coupon/dto/request/CreateCouponCategoryRequest.java
@@ -17,11 +17,11 @@ public record CreateCouponCategoryRequest(
 	@NotNull(message = "이름은 필수 입력 항목입니다.") @Size(max = 255, message = "이름은 255자 이하로 입력해야 합니다.") String name,
 	@NotNull(message = "할인 금액은 필수 입력 항목입니다.") @Range(min = 0, max = 100000) long discountAmount,
 	@NotNull(message = "할인 유형은 필수 입력 항목입니다.") DiscountType discountType,
-	@NotNull(message = "최대 할인 금액은 필수 입력 항목입니다.") long maxDiscountAmount,
-	@NotNull(message = "최소 구매 금액은 필수 입력 항목입니다.") long minPurchaseAmount,
+	@NotNull(message = "최대 할인 금액은 필수 입력 항목입니다.") Long maxDiscountAmount,
+	@NotNull(message = "최소 구매 금액은 필수 입력 항목입니다.") Long minPurchaseAmount,
 	@NotNull(message = "시작 날짜는 필수 입력 항목입니다.") LocalDateTime startedAt,
 	@NotNull(message = "만료 날짜는 필수 입력 항목입니다.") LocalDateTime expirationAt,
-	@NotNull(message = "사용 가능일은 필수 입력 항목입니다.") int usePeriod)
+	@NotNull(message = "사용 가능일은 필수 입력 항목입니다.") Integer usePeriod)
 	implements Discountable {
 	@AssertTrue(message = "시작 날짜는 만료 날짜보다 이전이어야 합니다.")
 	public boolean isValidDates() {
