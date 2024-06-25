5de8b11852f835ba9f8894c4874e6a18a18e2bd4
zaysverse <zeze@jbnu.ac.kr>
2024-06-25 13:48:02 +0900

    remove:

--- src/main/java/store/novabook/coupon/coupon/repository/CouponRepository.java
+++ src/main/java/store/novabook/coupon/coupon/repository/CouponRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import store.novabook.coupon.coupon.entity.Coupon;
 import store.novabook.coupon.coupon.entity.CouponStatus;
-import store.novabook.coupon.coupon.repository.querydsl.CustomizedMemberCouponRepository;
 
-public interface CouponRepository extends JpaRepository<Coupon, Long>, CustomizedMemberCouponRepository {
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
 	Page<Coupon> findAllByStatus(CouponStatus status, Pageable pageable);
 }
