7ceed5c4cbfd0cdc00ead8bba1e8e11dd6e460e4
zaysverse <zeze@jbnu.ac.kr>
2024-06-28 16:13:26 +0900

    cd: 배포

--- src/main/java/store/novabook/coupon/common/adapter/StoreAdapter.java
+++ src/main/java/store/novabook/coupon/common/adapter/StoreAdapter.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "store-service", url = "${store.service.url}")
+@FeignClient(name = "store-service")
 public interface StoreAdapter {
 
 	@GetMapping("/books/{bookId}")
