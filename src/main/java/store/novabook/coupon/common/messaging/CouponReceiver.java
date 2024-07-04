d1efdb45cffb2ce04b8504ebc34baceefa4a36f2
zaysverse <zeze@jbnu.ac.kr>
2024-07-04 23:22:33 +0900

    fix: 쿠폰 리시버 고침

--- src/main/java/store/novabook/coupon/common/messaging/CouponReceiver.java
+++ src/main/java/store/novabook/coupon/common/messaging/CouponReceiver.java
@@ -2,14 +2,12 @@ package store.novabook.coupon.common.messaging;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import store.novabook.coupon.common.exception.NovaException;
 import store.novabook.coupon.common.messaging.dto.CreateCouponMessage;
 import store.novabook.coupon.common.messaging.dto.RegisterCouponMessage;
-import store.novabook.coupon.common.security.aop.CurrentMembers;
 import store.novabook.coupon.coupon.service.CouponService;
 
 @Service
@@ -20,29 +18,20 @@ public class CouponReceiver {
 	private final CouponService couponService;
 	private final CouponNotifier couponNotifier;
 
+	// 웰컴 쿠폰 등록
 	@RabbitListener(queues = "${rabbitmq.queue.couponCreateHighTraffic}")
-	public void receiveCreateCouponHighTrafficMessage(@CurrentMembers Long clientId,
-		@Valid CreateCouponMessage message) {
-		try {
-			RegisterCouponMessage response = couponService.createByMessage(message);
-			couponSender.sendToRegisterHighTrafficQueue(response);        // 쿠폰 생성 후 회원 서버로 쿠폰 등록 메시지 전송
-		} catch (NovaException e) {
-			couponNotifier.notify(clientId.toString(), e.getMessage());
-		}
+	public void receiveCreateCouponHighTrafficMessage(@Valid CreateCouponMessage message) {
+		RegisterCouponMessage response = couponService.createByMessage(message);
+		couponSender.sendToRegisterHighTrafficQueue(response);
 	}
 
 	@RabbitListener(queues = "${rabbitmq.queue.couponCreateNormal}")
-	public void receiveCreateCouponNormalMessage(@CurrentMembers Long clientId, @Valid CreateCouponMessage message) {
+	public void receiveCreateCouponNormalMessage(@Valid CreateCouponMessage message) {
 		try {
 			RegisterCouponMessage response = couponService.createByMessage(message);
 			couponSender.sendToRegisterNormalQueue(response);        // 쿠폰 생성 후 회원 서버로 쿠폰 등록 메시지 전송
 		} catch (NovaException e) {
-			couponNotifier.notify(String.valueOf(clientId), e.getMessage());
+			couponNotifier.notify(message.memberId().toString(), e.getMessage());
 		}
-	}
-
-	@ExceptionHandler(Exception.class)
-	public void handleException(Exception ex, @CurrentMembers Long clientId) {
-		couponNotifier.notify(String.valueOf(clientId), "쿠폰 발급이 실패하였습니다.");
 	}
 }
