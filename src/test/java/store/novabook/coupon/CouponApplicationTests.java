2f52ed72ade0a80398a158f5f8bbcf96484354ef
Hantaehwan <gksxoghkskr@naver.com>
2024-07-08 10:30:41 +0900

    key-manager 설정

--- src/test/java/store/novabook/coupon/CouponApplicationTests.java
+++ src/test/java/store/novabook/coupon/CouponApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class CouponApplicationTests {
 	@Test
 	void contextLoads() {
