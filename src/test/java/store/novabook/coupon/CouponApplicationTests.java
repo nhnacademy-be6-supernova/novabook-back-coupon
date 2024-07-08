8061fe5213befcf5827556eeeab5595934219aec
Hantaehwan <gksxoghkskr@naver.com>
2024-07-08 16:10:14 +0900

    key-manager 설정

--- src/test/java/store/novabook/coupon/CouponApplicationTests.java
+++ src/test/java/store/novabook/coupon/CouponApplicationTests.java
@@ -1,12 +1,20 @@
 package store.novabook.coupon;
 
+import static org.hibernate.validator.internal.util.Contracts.*;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import store.novabook.coupon.coupon.repository.CouponRepository;
+
 @SpringBootTest
 class CouponApplicationTests {
+	@Autowired
+	private CouponRepository couponRepository;
 	@Test
 	void contextLoads() {
+		assertNotNull(couponRepository);
 	}
 }
