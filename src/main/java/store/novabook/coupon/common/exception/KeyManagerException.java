7489651e942225ae086fafe156732871d3414b56
Hantaehwan <gksxoghkskr@naver.com>
2024-07-17 15:30:21 +0900

    log crash info 제거

---
+++ src/main/java/store/novabook/coupon/common/exception/KeyManagerException.java
@@ -0,0 +1,7 @@
+package store.novabook.coupon.common.exception;
+
+public class KeyManagerException extends NovaException {
+	public KeyManagerException(ErrorCode errorCode) {
+		super(errorCode);
+	}
+}
